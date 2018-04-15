04405bf774045c5793457bb3b9eaa5d1c898f007
Joe da Silva <silvacore@hotmail.co.uk>
2018-04-15 16:29:06 +0100

    juiced up main menu; added an aura effect to the exit portal; added a custom cursor

---
+++ Assets/Behaviours/AttractCameraPivot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttractCameraPivot : MonoBehaviour
+{
+    [Header("Mouse Influence")]
+    [SerializeField] float horizontal_influence = 0.05f;
+    [SerializeField] float vertical_influence = 0.05f;
+
+    private Vector3 start_rot;
+
+
+    void Awake()
+    {
+        start_rot = transform.localEulerAngles;
+    }
+
+
+    void Update()
+    {
+        float x_rot = (Input.mousePosition.x / Screen.width) * horizontal_influence;
+        float y_rot = (Input.mousePosition.y / Screen.height) * -vertical_influence;
+
+        transform.localEulerAngles = start_rot + new Vector3(y_rot, x_rot, 0);
+    }
+
+}
